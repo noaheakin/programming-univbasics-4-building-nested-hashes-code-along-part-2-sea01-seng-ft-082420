1f3099ccdccde64530cfd75fc72f6862d77f38ad
Noah Eakin <noaheakin@gmail.com>
2020-07-18 16:59:42 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -1,9 +1,8 @@
-def update_status
-   epic_tragedy = {
+epic_tragedy = {
       :montague => {
          :patriarch => {name: "Lord Montague", age: "53"},
          :matriarch => {name: "Lady Montague", age: "54"},
-         :hero => {name: "Romeo", age: "15", status: "alive"},
+         :hero => {name: "Romeo", age: "15", status: "dead"},
          :hero_friends => [
             {name: "Benvolio", age: "17", attitude: "worried"},
             {name: "Mercutio", age: "18", attitude: "hot-headed"}
@@ -12,16 +11,25 @@ def update_status
       :capulet => {
          :patriarch => {name: "Lord Capulet", age: "50"},
          :matriarch => {name: "Lady Capulet", age: "51"},
-         :heroine => {name: "Juliet", age: "15", status: "alive"},
+         :heroine => {name: "Juliet", age: "15", status: "dead"},
          :heroine_friends => [
          {name: "Steven", age: "30", attitude: "confused"},
          {name: "Nurse", age: "44", attitude: "worried"}
          ]
-      }
-   }
+  }
+}
 
-  # Write code that changes the status of both Romeo and Juliet from "alive" to "dead"
-  # Code your solution here:
+def update_status
+  epic_tragedy[:montague][:hero][2] = "dead"
+  epic_tragedy[:capulet][:heroine][2] = "dead"
+end
+
+def add_characters
+  epic_tragedy[:additional_characters] = []
+  epic_tragedy[:additional_characters][0] = {:name => "Prince Escalus"}
+  epic_tragedy[:additional_characters][1] = {:name => "Apothecary"}
+end
+
 
 
 
@@ -56,8 +64,8 @@ def add_characters
    #
    # - Add an additional :additional_characters key to epic_tragedy and assign it an array as its value
    # - Add two hashes to this array, both with a :name key.
-   #   - The first hash should have the :name key set to "Prince Escalus" 
-   #   - The second hash should have the :name key set to "Apothecary" 
+   #   - The first hash should have the :name key set to "Prince Escalus"
+   #   - The second hash should have the :name key set to "Apothecary"
    # Code your solution here:
 
 
